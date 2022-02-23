ea70f548d674fc076ef012518b4d849d0d88cf99
Isabela <isaruizta2@gmail.com>
2022-02-23 08:00:14 -0500

    …

---
+++ taller1.js
@@ -0,0 +1,10 @@
+// taller1
+// fecha 23/02/22
+// realizado por: Maria Jose Henao, Julian Torres, Jeffer Rodriguez 
+
+var nombreestudiante="carlos"; //string 
+var grado="10A"; //string
+var edad="15"; //int
+var edad=prompt("el estudiante carlos del grado 10A tiene "+edad)
+
+
