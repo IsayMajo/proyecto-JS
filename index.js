be79cb8d5f400d5e405f37626bad85329b74d124
Isabela <isaruizta2@gmail.com>
2022-02-23 07:39:36 -0500

    …

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hola mundo");
+// comentarios en linea
+ /*comentario
+ de
+ varias
+ lineas*/
+ 
+
+//  variables
+var nombreusuario; 
+let edadusuario; 
+// declarar variable
+var nombrevariable; 
+// crear variable sin asignar valor
+/*inicializar variables:
+
+
+/*asignar un valor
+asignar un valor al momento de inicializar al momento de crear la variable
+*/
+var gradoestudiante=("10a"); 
+/* asignar un valor : 
+igualar una variable a un nuevo valor
+*/
+var edad=17;console.log(edadusuario);
+edadusuario=edad;
+
+console.log(edadusuario);
+
+/* asignar valor desd el teclado:
+
+existe dos maneras de hacerlos, la primera es por ventana emergente y la segunda es por medio de un formulario de html 
+*/
+
+var valor=prompt("ingrese un valor: ");
+console.log("la variable vale " + valor );
+ 
+/*constantes:
+son valores que no puden ser cambiados durante la ejecucion del codigo. Las constantes siempre deben ser inicializadas  
+*/
+const PI=3.1416; 
+// imprimir por consola
+// simple
+console.log("hola");
+console.log(grado)
+// condenado
+console.log(""+valor)
