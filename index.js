4dd23277c0379ce354a7bfb487620b0368b1f7ee
Isabela <isaruizta2@gmail.com>
2022-03-08 13:19:44 -0500

    soluciondeltaller2

--- index.js
+++ index.js
@@ -42,6 +42,75 @@ const PI=3.1416;
 // imprimir por consola
 // simple
 console.log("hola");
+var grado = "10a";
 console.log(grado)
 // condenado
 console.log(""+valor)
+// operadores 
+// suma 
+var suma;
+suma = 5+4;
+console.log(suma)
+var a = 12;
+suma = a+8;
+console.log(suma);
+var b = 20;
+suma = a+b;
+console.log(suma);
+// resta
+var resultado;
+resultado = a-b;
+console.log(resultado);
+resultado = b-a;
+console.log(resultado);
+// multiplicacion
+var producto;
+producto = a*b;
+console.log(producto);
+// division
+var division;
+division = a/b;
+console.log(division);
+division = b/a;
+console.log(division);
+// orden de operacion
+var result;
+result = a*b;
+console.log(result);
+result = a*b-10;
+console.log(result);
+result = a*(b-10);
+console.log(result);
+// exponente/potencia
+var exp = 3**2;
+console.log(exp);
+exp = 3**3;
+console.log(exp);
+// cadenas o string
+var cadenita1 = "3";
+var cadenita2 = "5";
+var sumacadenas = cadenita1 + cadenita2;
+console.log(sumacadenas);
+
+var nombrecito = "Isabela";
+var apellidito = "Ruiz";
+var nomcompleto = nombrecito + "  " + apellidito;
+console.log(nomcompleto);
+var longitud = nombrecito.length;
+console.log(longitud);
+console.log(nomcompleto.length);
+//encontrar caracteres dada la posicion en el vector
+var primeraletra;
+primeraletra = nombrecito[0];
+console.log(primeraletra);
+console.log (nombrecito[1]);
+//encontrar la ultima posicion de una cadena o vector
+console.log(nombrecito[nombrecito.length - 1]);
+ //se debe buscar la longitud de la cadena de restarle uno para encontrar la ultima letra
+ //valores inmutables
+ //las posiciones de una cadena se pueden consultar pero no reasignar su valor
+ nombrecito[0] = "H";
+ console.log(nombrecito);
+ nombrecito = "Maria";
+ console.log(nombrecito);
+ 
--- taller1.js
+++ taller1.js
@@ -1,6 +1,6 @@
 // taller1
 // fecha 23/02/22
-// realizado por: Maria Jose Henao, Julian Torres, Jeffer Rodriguez 
+// realizado por: Maria Jose Henao, Isabela Ruiz, Julian Torres, Jeffer Rodriguez 
 
 var nombreestudiante="carlos"; //string 
 var grado="10A"; //string
