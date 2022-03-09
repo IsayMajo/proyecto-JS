40e5d7160fed33c3e5bcb7cfaceaa0ead62d2fe2
Isabela <isaruizta2@gmail.com>
2022-03-09 07:01:25 -0500

    soluciondeltaller2

---
+++ taller2.js
@@ -0,0 +1,9 @@
+//halle el area del cuadrado y el volumen del cubo
+var areayvolumen = "halle el area de un cuadrado y el volumen de un cubo"
+console.log(Num1)
+ // Area del Cuadrado
+var Area1 = Alt * Bas
+ var Bas=prompt("valor de la base: ");
+ var Alt=prompt("valor de la altura: ");
+ 
+console.log("el area del cuadrado es" + Area1)
