ef45d8edd0e36b67984af3f32b2792102214582a
Isabela <isaruizta2@gmail.com>
2022-03-09 07:57:15 -0500

    soluciondeltaller2

--- taller2.js
+++ taller2.js
@@ -1,9 +1,35 @@
 //halle el area del cuadrado y el volumen del cubo
 var areayvolumen = "halle el area de un cuadrado y el volumen de un cubo"
-console.log(Num1)
  // Area del Cuadrado
-var Area1 = Alt * Bas
- var Bas=prompt("valor de la base: ");
- var Alt=prompt("valor de la altura: ");
+var Area1 = Alt * Alt
+ var Lad=prompt("valor de un lado: ");
  
-console.log("el area del cuadrado es" + Area1)
+console.log("el area del cuadrado es " + Lad * Lad)
+var volum1 = lad1 * lad2 * lad3 
+var lad1=prompt("coloque el valor del primer lado: ");
+var lad2=prompt("coloque el valor del segundo lado: ");
+var lad3=prompt("coloque el valor del tercer lado: ");
+console.log("el volumen del cubo es " + lad1 * lad2 * lad3  )
+
+//hallar el area de un rectangulo
+var Alt=prompt("coloque el valor de la altura:  "); 
+var Bas=prompt("coloque el valor de la base:  ");
+console.log("el area del cuadrado del rectangulo es " + Alt * Bas)
+
+//hallar el perimetro de un rectangulo
+var perime = ladd + ladd2 + ladd3 + ladd4
+var ladd=prompt("ponga el valor del lado del rectangulo: ");
+var ladd2=prompt("ponga el valor del segundo lado del rectangulo: ");
+var ladd3=prompt("ponga el valor del tercer lado del rectangulo: ");
+var ladd4=prompt("ponga el valor del cuarto lado del rectangulo: ");
+console.log("el perimetro del rectangulo " +  ladd + ladd2 + ladd3 + ladd4)
+
+//area y volumen del circulo
+var areadecirculo = rd * rd * 3.1416
+var rd=prompt("valor del radio:  ");
+console.log("el area de un circulo es " + rd * rd * 3.1416) 
+var volumfera;
+volumfera = prompt ( "ingrese el valor del radio de la esfera " );
+volumfera = ( 4 / 3 ) * 3.1416 * 3;
+console.log ( " el volumen de su esfera es ", volumfera ); 
+
