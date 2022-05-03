480d479c4a9624017f18e2069f3a64a88b0ba465
Isabela <isaruizta2@gmail.com>
2022-05-03 13:39:42 -0500

    taller3.js

---
+++ evaluaciocondicionales.js
@@ -0,0 +1,66 @@
+// Isabela Ruiz Tabares y Maria Jose Henao S
+
+//punto 1
+
+var ValorVariable= 200;
+ 
+if (ValorVariable < 250 ){
+   console.log("La condición es verdadera")
+ }
+
+ //punto 2
+
+ var valor = 5;
+ 
+if (valor >= 60 || valor <= 10){
+   console.log("El valor es correcto")
+}
+
+//punto 3
+
+var numero;
+ 
+if ( numero === 20 ){
+   console.log('El número es igual que "20" en valor y tipo');
+}else if( numero > 30 && numero < 40){
+    console.log('El número es mayor que 30 y menor a 40');
+}else if( numero <= 30 ){
+    console.log("El número es igual que 30 o menor igual que 29");
+}else if( numero >= 40){
+    console.log("El número es mayor igual que 40");
+}
+
+// continuacion 3
+//a 
+numero = 20
+//b
+numero = 35
+//c
+numero = 30
+//d
+numero = 40
+
+//punto 4
+
+var numero1 = 5;
+var numero2 = 10;
+var numero3 = "suma" 
+if (numero3 == "suma") {
+console.log( numero1 + numero2 ); }
+else if ( numero3 == "resta") {
+console.log( numero1 - numero2 ); }
+else if ( numero3 == "multiplicacion") {
+console.log( numero1 * numero2); }
+
+//punto 5
+
+var producto = "salchipapa" 
+var precio 
+if ( producto == "sandwich" ) {
+console.log("el sandwich le cuesta 2500"); }
+else if ( producto == "churro" ) {
+console.log(" le cuesta 2000"); }
+else if ( producto == "salchipapa" ) {
+console.log(" le cuesta 2800 "); }
+else if ( producto == "empanada" ) {
+console.log(" le cuesta 1000 "); }
