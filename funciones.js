b2ad39eb666c8d639375e13810d1fac8c9410894
Isabela <isaruizta2@gmail.com>
2022-11-11 11:26:08 -0500

    segundo  taller cuarto periodo

--- funciones.js
+++ funciones.js
@@ -1,43 +1,44 @@
-console.log(" hola ");
+console.log("hola");
 
 function saludo(){
-    console.log(" que tal? ");
+    console.log("¿que tal?");
 }
-//llamado a la funcion
+//llamado a la funcion(mala)
 saludo();
 function suma(){
-    var numero1 = 2;
-    var numero2 = 4;
-    console.log(numero1+numero2);
+var numero1=2;
+var numero2=4;
+console.log(numero1+numero2);
 }
-suma()
+//nuena 
 function sumita(){
     var numerito1=10;
     var numerito2=12;
     return numerito1+numerito2;
+    
 }
 console.log(sumita());
 
 //funcion con parametros
-function sumar(num1, num2){
+function sumar (num1,num2) {
     return num1 + num2;
 }
 console.log(sumar(3, 8));
-
-//diseñe un algoritmo que retorne el doble de la suma entre dos numeros
-
-sumanumeros = sumar(1, 2);
-function doblenumero(sumanumeros){
-return sumanumeros * 2;
+//diseñe un algoritmo que retorne el doble de la 
+//suma entre dos numeros
+sumaNumeros=sumar(1,2);
+function dobleNumero (sumaNumeros){
+    return sumaNumeros*2;
 }
-console.log(" el doble de la suma es: " + doblenumero(sumanumeros));
+console.log("el doble de la suma es:"+ dobleNumero(sumaNumeros));
 
-/*solucion 2 una solucion puede ser llamada por otra.
-*/
-function doblenumero2(num1, num2){
-    return sumar(num1, num2) *2;
+/*SOLUCION 2
+una funcion puede  ser llamada por otra
+*/ 
+function dobleNumero(num1, num2){
+    return sumar (num1, num2)* 2;
 }
 
-var resultado = doblenumero2 (5, 10);
-console.log (" el doble de la suma de dos numeros es " + resultado);
 
+var resultado = dobleNumero (5, 10);
+console.log(" doble de la suma de dos numeros es " + resultado);
